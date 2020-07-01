142f8229cd796bd40506b18c9271fcd34515e59d
DANIL ORLOV <stolichna9@yandex.ru>
2020-07-01 09:55:20 +0700

    Added qiwi api. Checking deposit.

--- config.go
+++ config.go
@@ -3,4 +3,5 @@ package main
 type Config struct {
 	TelegramToken string `json:"telegramToken"`
 	QiwiToken     string `json:"qiwiToken"`
+	QiwiWallet    string `json:"qiwiWallet"`
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/tkanos/gonfig"
+	"github.com/zhuharev/qiwi"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -50,6 +51,8 @@ func main() {
 	log.Println("Connected to MongoDB!")
 
 	userCollection := client.Database("store").Collection("user")
+
+	qw := qiwi.New(config.QiwiToken, qiwi.Wallet(config.QiwiWallet))
 
 	// Connect to a telegram bot
 	bot, err := tgbotapi.NewBotAPI(config.TelegramToken)
@@ -121,7 +124,9 @@ func main() {
 		case "myaccauntmenu":
 			switch update.Message.Command() {
 			case "deposit":
-				reply = "You are in deposit menu."
+				reply = "You need to send money to qiwi wallet: " + config.QiwiWallet + "\n"
+				reply += "Type in comment field you telegram id: " + strconv.Itoa(update.Message.From.ID)
+				reply += "Then press the button /checkdeposit"
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 				bot.Send(msg)
 				menunav = "depositmenu"
@@ -140,7 +145,47 @@ func main() {
 				bot.Send(msg)
 				menunav = "mainmenu"
 			}
-
+		case "depositmenu":
+			switch update.Message.Command() {
+			case "checkdeposit":
+				var user User
+				paymentsHistory, err := qw.Payments.History(context.Background(), 10)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for i := 0; i < 10; i++ {
+					if paymentsHistory.Data[i].Comment == strconv.Itoa(update.Message.From.ID) {
+						filterUser := bson.D{{"username", update.Message.From.UserName}, {"telegramID", update.Message.From.ID}}
+						err := userCollection.FindOne(context.TODO(), filterUser).Decode(&user)
+						if err != nil {
+							log.Fatal(err)
+						}
+						updateUser := bson.D{{"$set",
+							bson.D{
+								{"balance", user.Balance + paymentsHistory.Data[i].Sum.Amount},
+							},
+						}}
+						_, err := userCollection.UpdateOne(context.TODO(), filterUser, updateUser)
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+				}
+			case "mainmenu":
+				reply = "You are in main menu."
+				buttons := make([][]tgbotapi.KeyboardButton, 2)
+				buttons[0] = tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("/items"), tgbotapi.NewKeyboardButton("/myaccaunt"))
+				buttons[1] = tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("/mainmenu"))
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+				msg.ReplyMarkup = tgbotapi.ReplyKeyboardMarkup{
+					Keyboard:        buttons,
+					ResizeKeyboard:  true,
+					OneTimeKeyboard: true,
+					Selective:       false,
+				}
+				bot.Send(msg)
+				menunav = "mainmenu"
+			}
 		}
 
 	}
